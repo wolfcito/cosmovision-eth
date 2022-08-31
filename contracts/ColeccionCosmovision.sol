b973d47b5453159e5e553d74fe56a5f9c05ee88d
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-30 21:06:13 -0500

    add contract for 'Colección cosmovision'

---
+++ contracts/ColeccionCosmovision.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// "Comunidad Kichwa"
+// "KICHWA"
+contract ColeccionCosmovision is ERC721("Comunidad Kichwa", "KICHWA") {
+    constructor() {}
+}
