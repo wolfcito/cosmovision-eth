4c3f8cd274482bfa5f8574bd9795fa151a0019f1
Luis Fernando Ushiña <guffenix@gmail.com>
2022-09-05 20:22:47 -0500

    adding historiographer zone

--- frontend/components/Header/HeaderLinks.js
+++ frontend/components/Header/HeaderLinks.js
@@ -1,29 +1,29 @@
 /*eslint-disable*/
-import React from "react";
-import Link from "next/link";
+import React from 'react'
+// import Link from 'next/link'
 
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Tooltip from "@material-ui/core/Tooltip";
-import Icon from "@material-ui/core/Icon";
+import { makeStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+// import Tooltip from '@material-ui/core/Tooltip'
+// import Icon from '@material-ui/core/Icon'
 
 // @material-ui/icons
-import { Apps, CloudDownload } from "@material-ui/icons";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
+// import { Apps, CloudDownload } from '@material-ui/icons'
+// import DeleteIcon from '@material-ui/icons/Delete'
+// import IconButton from '@material-ui/core/IconButton'
 
 // core components
-import CustomDropdown from "/components/CustomDropdown/CustomDropdown.js";
-import Button from "/components/CustomButtons/Button.js";
+// import CustomDropdown from '/components/CustomDropdown/CustomDropdown.js'
+import Button from '/components/CustomButtons/Button.js'
 
-import styles from "/styles/jss/nextjs-material-kit/components/headerLinksStyle.js";
+import styles from '/styles/jss/nextjs-material-kit/components/headerLinksStyle.js'
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
 export default function HeaderLinks(props) {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <List className={classes.list}>
       <ListItem className={classes.listItem}>
@@ -36,23 +36,24 @@ export default function HeaderLinks(props) {
         </Button>
       </ListItem>
       <ListItem className={classes.listItem}>
-        <Button
-          href="/about"
-          color="transparent"
-          className={classes.navLink}
-        >
+        <Button href="/about" color="transparent" className={classes.navLink}>
           About us
         </Button>
       </ListItem>
       <ListItem className={classes.listItem}>
         <Button
-          href="/roadmap"
+          href="/historiographer"
           color="transparent"
           className={classes.navLink}
         >
+          Historiographer
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button href="/roadmap" color="transparent" className={classes.navLink}>
           Roadmap
         </Button>
       </ListItem>
     </List>
-  );
+  )
 }
